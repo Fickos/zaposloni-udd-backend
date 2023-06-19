70674d57adffcba6d2c7da1bdffb8f908c3d9a6b
Fickos <filip.volaric99@gmail.com>
2023-06-19 21:41:24 +0200

    Partial setup of node application.

---
+++ config.js
@@ -0,0 +1,6 @@
+const DB_URI = "mongodb://localhost:27017";
+const DB_NAME = "genereactoni";
+
+const PORT = process.env.port || 3000;
+
+module.exports = { DB_URI, DB_NAME, PORT };
---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(express.json());
+
+const whitelist = ["http://localhost:3001"];
+const corsOptions = {
+  origin: function(origin, callback) {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error("Not allowed by CORS"))
+    }
+  },
+  credentials: true
+}
+
+app.use(cors(corsOptions));
+
+// const userRouter = require('./routes/users.route');
+// const modelRouter = require('./routes/model.route');
+
+// app.use('/users', userRouter);
+// app.use('/model', modelRouter);
+const routes = require('./routes');
+app.use('', routes);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  console.error(err.message, err.stack);
+  res.status(statusCode).json({'message': err.message});
+  
+  return;
+});
+
+const config = require('./config');
+
+app.listen(config.PORT, console.log(`Listening on port ${config.PORT}...`));
